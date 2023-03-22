629b8b12e6448fdca27e997c1979a783c7d0850e
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-03-22 12:04:49 -0300

    Add actions

---
+++ .github/workflows/main.py
@@ -0,0 +1,22 @@
+name: scraping_rightmove
+
+on:
+  # schedule:
+  #   - cron: '0 11 23 * *' # set in UTC timezone - https://crontab.guru - 11 UTC = 8 SP
+  workflow_dispatch
+
+jobs:
+  scraping-rightmove:
+    name: Scraping Rightmove
+    runs-on: ubuntu-latest    
+    steps:
+      - name: Checkout repository
+        uses: actions/checkout@v3
+        # with: # Only if private repos
+        #   token: ${{ secrets.GH_TOKEN }}
+      - name: Install packages
+        run: pip install -r requirements.txt
+      - name: Scraping Rightmove
+        run: python main.py
+      - name: Commit
+        uses: stefanzweifel/git-auto-commit-action@v4
